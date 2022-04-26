2c89f08dd1dacffc9a0b304c8eb243d959e6bac4
Ivan Marinin <90220027+ISMarinin@users.noreply.github.com>
2022-04-26 11:18:15 +0300

    Added pop-up window appearance

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const apiKey = '2816fe41-4647-4f8f-84b7-733b04bd95b8'
 const apiUrlPopular = 'https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=TOP_100_POPULAR_FILMS&page='
 const apiUrlSearch = 'https://kinopoiskapiunofficial.tech/api/v2.1/films/search-by-keyword?keyword='
+const apiUlDescription = 'https://kinopoiskapiunofficial.tech/api/v2.2/films/'
 
-async function getMovies(url) {
+async function getMovies(url) {         // функция, которая получает данные о фильме
     const resp = await fetch(url, {
         headers: {
             'Content-Type': 'application/json',
@@ -13,7 +14,7 @@ async function getMovies(url) {
     showMovies(respData)
 }
 
-function showMovies(data) {
+function showMovies(data) {         //функция, которая показывает список фильмов
     const moviesEl = document.querySelector('.movies')
 
     document.querySelector('.movies').innerHTML = ''
@@ -22,7 +23,7 @@ function showMovies(data) {
         const movieEl = document.createElement('div')
         movieEl.classList.add('movie')
         movieEl.innerHTML = `
-            <div class="movie">
+            <div class="movie popup__link" id="${movie.filmId}">
                 <div class="movie__cover-inner">
                     <img src="${movie.posterUrlPreview}" class="movie__cover" alt="${movie.nameRu}">
                     <div class="movie__cover-darkened"></div>
@@ -40,10 +41,10 @@ function showMovies(data) {
             </div>
         `
         moviesEl.appendChild(movieEl)
+        // console.log(movie.filmId)
     })
 }
-
-async function getPages(url) {
+async function getPages(url) {      // функция, получающая даные о количестве страниц
     const resp = await fetch(url, {
         headers: {
             'Content-Type': 'application/json',
@@ -54,7 +55,7 @@ async function getPages(url) {
     countPages(respData)
 }
 
-function countPages(data) {
+function countPages(data) {     // функция, которая выводит страницы
 
     const btn = document.querySelector('.btns__pages')
 
@@ -68,18 +69,47 @@ function countPages(data) {
     }
 }
 
-function getColor(data) {
+function getColor(data) {       // получение цвета для рейтинга
     if (data >= 7) return 'green'
     else if (data < 5) return 'red'
     else return 'orange'
 }
 
+async function getDescription(url) {        // функция, которая получает подробное описание фильма
+    const resp = await fetch(url, {
+        headers: {
+            'Content-Type': 'application/json',
+            'X-API-KEY': apiKey
+        }
+    })
+    const respData = await resp.json()
+    showDescription(respData)
+}
+
+function showDescription(data) {        // показываем описание в виде попап
+    const popupEl = document.querySelector('.popup')
+
+
+    document.querySelector('.popup').innerHTML = ''
+
+    const popupBody = document.createElement('div')
+    popupBody.classList.add('popup__body')
+    popupEl.innerHTML = `
+        <div class="popup__body"> 
+            <div class="popup__content">
+                <a class="popup__close" id="popup__close">&times;</a>
+                <div class="popup__title">${data.nameRu}</div>
+                <div class="popup__text">${data.description}</div>
+            </div>
+        </div>
+    `
+}
 
 const form = document.querySelector('form')
 const search = document.querySelector('.header__search')
 
-form.addEventListener('submit', err => {
-    err.preventDefault()
+form.addEventListener('submit', e => {      // поиск по форме
+    e.preventDefault()
 
     const apiSearchUrl = `${apiUrlSearch}${search.value}`
     if (search.value) {
@@ -89,23 +119,48 @@ form.addEventListener('submit', err => {
     search.value = ''
 })
 
-getMovies(apiUrlPopular)
-getPages(apiUrlPopular)
+getMovies(apiUrlPopular).then(() => {       // вызываем getMovies
+    getPages(apiUrlPopular)                 //вызываем getPages
 
-const page = document.querySelector('.btns__pages')
-const num = document.querySelector('.num__page')
-
-page.addEventListener('click', e => {
-    e.preventDefault()
-    const { target } = e;
-    const apiPageUrl = `${apiUrlPopular}${target.innerHTML}`
-
-    if(target.innerHTML) {
-        getMovies(apiPageUrl)
-        window.scrollBy(0, -2000)
-        target.classList.add('num__active')
-        // console.log(target.innerHTML)
-    }
-    // target.classList.remove('num-active')
+    const page = document.querySelector('.btns__pages')
     
+    page.addEventListener('click', e => {           // переключение страницы при клике
+        e.preventDefault()
+        const { target } = e;
+        const apiPageUrl = `${apiUrlPopular}${target.innerHTML}`
+    
+        if(target.innerHTML) {
+            getMovies(apiPageUrl)
+            window.scrollBy(0, -2000)
+            target.classList.add('num__active')
+        }
+        const num = document.querySelector('.num__active')
+        num.classList.remove('num__active')
+    })
+    
+    const popupOpen = document.querySelectorAll('.popup__link')  // получаем массив div'ов с классом popup__link
+    const popupClose = document.querySelector('.popup__close')
+    const popup = document.querySelector('.popup')
+
+    for(let i = 0; i < popupOpen.length; ++i) {         // в for мы ищем элемент, на который кликнули
+        openPopup = popupOpen[i]
+        openPopup.addEventListener('click', e => {
+            e.preventDefault()
+            popup.classList.add('active')
+            const id = e.path[2].getAttribute('id')     // берем его id
+            console.log(id)
+            const urlId = `${apiUlDescription}${id}` 
+            getDescription(urlId)                       // подаем ссылку с описанием фильма в getDescription
+        })
+    }
+
+    popupClose.addEventListener('click', e => {         // закрываем попап при нажатии на крестик
+        popup.classList.remove('active')               
+    })
 })
+
+/*
+    я думаю, что попап не закрывается, потому что ассинхронная функция getDescription(urlId) 
+    отправляется в очередь, а функция закрытия выполняется синхронно, т.е. сразу,
+    но к моменту ее выполнения еще нечего закрывать
+*/
